2721be4ebb3f8e026f775a1a211d25645a1fd263
Soujit Saha <soujitsaha9083@gmail.com>
2023-02-16 13:31:59 +0530

    Login,singup

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -3,13 +3,17 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import AddProduct from '../screens/AddProduct';
+import Login from '../screens/Login';
+import Singup from '../screens/Singup';
 
 const StackNav = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator initialRouteName="Home" headerMode="none">
+    <Stack.Navigator initialRouteName="Login" headerMode="none">
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="AddProduct" component={AddProduct} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Singup" component={Singup} />
     </Stack.Navigator>
   );
 };
